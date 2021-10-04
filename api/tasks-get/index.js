9f5ed16fb736d35fe38b33c8e19109334600984f
LMERLAUD <louis.merlaud@ynov.com>
2021-10-04 16:35:29 +0200

    alu

--- api/tasks-get/index.js
+++ api/tasks-get/index.js
@@ -5,28 +5,26 @@ module.exports = async function (context, req) {
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-        const tasks = [
-            {
-                id: 1,
-                label: "🍔 Eat",
-                status: ""
-            },
-            {
-                id: 2,
-                label: "🛏 Sleep",
-                status: ""
-            },
-            {
-                id: 3,
-                label: "</> Code",
-                status: ""
-            }
-        ];
-
+        
+    const tasks = [
+        {
+            id: 1,
+            label: "🍔 Eat",
+            status: ""
+        },
+        {
+            id: 2,
+            label: "🛏 Sleep",
+            status: ""
+        },
+        {
+            id: 3,
+            label: "</> Code",
+            status: ""
+        }
+    ];
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: tasks
     };
-    return tasks
 }
